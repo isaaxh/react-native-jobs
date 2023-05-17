b9ea5725174a1edff7d100316fdac00871ec51aa
Isaac Hussain <isaaclearns58@gmail.com>
2023-05-17 12:26:13 +0300

    import necassary items for JobDetails page

---
+++ app/job-details/[id].js
@@ -0,0 +1,27 @@
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
+
+const JobDetails = () => {
+  return <Text>JobDetails</Text>;
+};
+
+export default JobDetails;
