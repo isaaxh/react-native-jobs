f3a6766697ff3cb4151088fb78b8bee81cd88202
Isaac Hussain <isaaclearns58@gmail.com>
2023-05-17 09:44:58 +0300

    add error handling code for api request in useFetch

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -18,4 +18,20 @@ const useFetch = (endpoint, query) => {
     },
     params: { ...query },
   };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 };
