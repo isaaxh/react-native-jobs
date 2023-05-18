24380a8fe330162fc868a1c1568e84a914db4075
Isaac Hussain <isaaclearns58@gmail.com>
2023-05-18 05:30:51 +0300

    qualifications tab done  for details jobs

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -35,6 +35,22 @@ const JobDetails = () => {
 
   const onRefresh = () => {};
 
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title='Qualifications'
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "About":
+      case "Responsibilities":
+      default:
+        break;
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -83,6 +99,8 @@ const JobDetails = () => {
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
               />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
